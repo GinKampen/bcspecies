16f5d039657897f2fd275e39cffaa9f6baf8e720
Gin Kampen <virginiakampen@gmail.com>
2020-04-12 12:53:24 -0700

    clean up

---
+++ R/demo.R
@@ -0,0 +1,14 @@
+#' double
+#' @param num the number to be doubled
+#' @return the value for num doubled 
+#' @export
+#' 
+#' @examples
+#' double(2) 
+#' 
+#' 
+
+double <- function(num){
+  result <- num * 2
+  result
+}
