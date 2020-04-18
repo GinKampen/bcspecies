ee60abf96e6bc7b81fb81400276984bd0672eade
Gin Kampen <virginiakampen@gmail.com>
2020-04-18 13:13:12 -0700

    test for species_map

--- tests/testthat/test-species.R
+++ tests/testthat/test-species.R
@@ -8,3 +8,14 @@ test_that("conservation_status works", {
   
 })
 
+test_that("species_map works", {
+  
+  map <- species_map("Anemone occidentalis - Carex nigricans")
+  
+  expect_true(inherits(map, c("gg", "ggplot")))
+  expect_identical(map$labels$subtitle, "Anemone occidentalis - Carex nigricans")
+  expect_identical(names(map$plot_env), c("gp", "species_present", "species_ecosections_spatial", 
+                                        "species_ecosections", "bc_boundary", "ecosection_simple", 
+                                        "ecosections", "species"))
+  
+})
